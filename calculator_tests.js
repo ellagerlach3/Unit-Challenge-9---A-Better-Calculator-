5e7a5379c508454e92bd84c3858eb8b89f89299f
Carolyn GERLACH <EllaGerlach@Carolyns-MacBook-Air.local>
2017-02-02 08:14:45 -0800

    all clear instead of clear current

--- calculator_tests.js
+++ calculator_tests.js
@@ -1,6 +1,6 @@
 // Test for inputing digits
 QUnit.test( "Add digits test", function( assert ) {
-    clearCurrent();
+    allClear();
     addDigit('1');
     addDigit('2');
     assert.equal(document.getElementById("screen").value, "12", "Passed - Expected 12");
@@ -8,12 +8,13 @@ QUnit.test( "Add digits test", function( assert ) {
 
 // Test adding one, then two decimals
 QUnit.test( "Add decimal test", function( assert ) {
-    clearCurrent();
+    allClear();
     addDecimal();
     addDigit('2');
     addDecimal();
     assert.equal(document.getElementById("screen").value, "0.2", "Passed - Expected 0.2");
 });
+<<<<<<< HEAD
 
 // Test for changing the current input to a percentage
 QUnit.test( "Percentage test", function( assert ) {
@@ -40,3 +41,37 @@ QUnit.test( "Calculating square test", function( assert ) {
 });
 
 
+=======
+// Test for changing the sign of the current input
+QUnit.test( "Change sign test", function ( assert ) {
+    allClear();
+    addDigit('1');
+    changeSign();
+    assert.equal(document.getElementById("screen").value, "-1", "Passed - Expected -1");
+});
+// Test for finding the inverse
+QUnit.test( "Inverse test", function ( assert ) {
+    allClear();
+    addDigit('5');
+    inverse();
+    assert.equal(document.getElementById("screen").value, "0.2", "Passed - Expected 0.2");
+});
+// Test for finding square root
+QUnit.test( "Square root test", function ( assert ) {
+    allClear();
+    addDigit('4');
+    squareRoot();
+    assert.equal(document.getElementById("screen").value, "2", "Passed - Expected 2");
+});
+// Test for clearing JUST current input, not the stored procedure
+QUnit.test( "Clear current input only test", function ( assert ) {
+    allClear();
+    addDigit('3');
+    storeOperator('+');
+    addDigit('5');
+    clearCurrent();
+    addDigit('2');
+    calculate();
+    assert.equal(document.getElementById("screen").value, "5", "Passed - Expected 5");
+});
+>>>>>>> origin/master
