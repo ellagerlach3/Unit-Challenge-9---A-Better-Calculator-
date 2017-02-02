050d7efb9691cc6ffd92e69188a3982dff3c564f
Carolyn GERLACH <EllaGerlach@Carolyns-MacBook-Air.local>
2017-02-02 08:17:18 -0800

    modified

--- calculator_tests.js
+++ calculator_tests.js
@@ -14,12 +14,10 @@ QUnit.test( "Add decimal test", function( assert ) {
     addDecimal();
     assert.equal(document.getElementById("screen").value, "0.2", "Passed - Expected 0.2");
 });
-<<<<<<< HEAD
-<<<<<<< HEAD
 
 // Test for changing the current input to a percentage
 QUnit.test( "Percentage test", function( assert ) {
-    clearCurrent();
+    allClear();
     addDigit('50');
     percentage();
     assert.equal(document.getElementById("screen").value, "0.5", "Passed - Expected 0.5");
@@ -27,7 +25,7 @@ QUnit.test( "Percentage test", function( assert ) {
 
 // Test for calcualtiong factorial of current input
 QUnit.test( "Calculating factorial test", function( assert ) {
-    clearCurrent();
+    allClear();
     addDigit('5');
     factorial();
     assert.equal(document.getElementById("screen").value, "120", "Passed - Expected 120");
@@ -35,16 +33,12 @@ QUnit.test( "Calculating factorial test", function( assert ) {
 
 // Test for calcualtiong the square of the current input
 QUnit.test( "Calculating square test", function( assert ) {
-    clearCurrent();
+    allClear();
     addDigit('7');
     square();
     assert.equal(document.getElementById("screen").value, "49", "Passed - Expected 49");
 });
 
-
-=======
-=======
->>>>>>> origin/master
 // Test for changing the sign of the current input
 QUnit.test( "Change sign test", function ( assert ) {
     allClear();
@@ -52,6 +46,7 @@ QUnit.test( "Change sign test", function ( assert ) {
     changeSign();
     assert.equal(document.getElementById("screen").value, "-1", "Passed - Expected -1");
 });
+
 // Test for finding the inverse
 QUnit.test( "Inverse test", function ( assert ) {
     allClear();
@@ -59,6 +54,7 @@ QUnit.test( "Inverse test", function ( assert ) {
     inverse();
     assert.equal(document.getElementById("screen").value, "0.2", "Passed - Expected 0.2");
 });
+
 // Test for finding square root
 QUnit.test( "Square root test", function ( assert ) {
     allClear();
@@ -66,6 +62,7 @@ QUnit.test( "Square root test", function ( assert ) {
     squareRoot();
     assert.equal(document.getElementById("screen").value, "2", "Passed - Expected 2");
 });
+
 // Test for clearing JUST current input, not the stored procedure
 QUnit.test( "Clear current input only test", function ( assert ) {
     allClear();
@@ -77,7 +74,3 @@ QUnit.test( "Clear current input only test", function ( assert ) {
     calculate();
     assert.equal(document.getElementById("screen").value, "5", "Passed - Expected 5");
 });
-<<<<<<< HEAD
->>>>>>> origin/master
-=======
->>>>>>> origin/master
